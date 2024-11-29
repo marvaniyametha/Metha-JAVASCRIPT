8ff60e19c52084631b4e8d1f7e834feb12b1a821
marvaniyametha <marvaniyametha07@gmail.com>
2024-11-29 16:02:52 +0530

    Project-1  25/11/2024

---
+++ basic calc .js
@@ -0,0 +1,141 @@
+// 1.write a javascript to find area of triangle.
+
+// formula:-1/2*b*h
+
+// var b=30;
+// var h=20;
+// var ans=1/2*b*h; 
+//  console.log("area of triangle = "+ans);
+
+// output:-
+//  area of triangle = 300
+
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+// 2.write a javascript program to convert tempreratures to and from celcius fahrenheit.
+
+
+// var c=20;
+// var d=(c*9/5)+32; //(0°C × 9/5) + 32 = 32°F
+// console.log("celsius to Fahrenheit ans is  = "+d);
+
+// output:-
+// celsius to Fahrenheit ans is  = 68
+// -------------------------------------------------------------------------------------------------------------------------------------------
+
+// 3.write a javascript to find the area of a recteangle.
+
+
+//   fromula:- l*w
+
+// var l=10;
+// var w=20;
+// var ans=l*w; 
+// console.log(" area of Rectangle = "+ans)
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// 4. write a javascript to find the area of a circle.
+
+// formula :- pi*r*r
+
+// var c=20;
+// var ans=3.14*c;
+// console.log("area of circle = "+ans);
+
+
+// 5.write a javascript program to convert to feet into inches.
+
+//  formula:-f*12
+
+// var f=5;
+// var ans=f*12;
+// console.log("feet to inches = "+ans);
+
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// 6) Wrtie a Javascript program to calculate given formula :-
+// i) (a^2) - (b^2) = (a-b)(a+b)
+
+// var a = 7, b = 3;
+// var lhs1 = a ** 2 - b ** 2;
+// var rhs1 = (a - b) * (a + b);
+
+// console.log("LHS : ", lhs1);
+// console.log("RHS : ", rhs1);
+
+// console.log("Are LHS and RHS are Equal?", lhs1 === rhs1);
+
+// Output:-       LHS :  40
+            //    RHS :  40
+            //    Are LHS and RHS are Equal? true
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// ii) (a-b)(a-b) = a*2 + b*2 - 2ab
+// let m = 5, n = 3;
+// let lhs2 = (m - n) ** (m - n);
+// let rhs2 = m ** 2 + n ** 2 - 2 * m * n;
+
+// console.log("LHS : ", lhs2);
+// console.log("RHS : ", rhs2);
+
+// console.log("Are LHS and RHS are Equal?", lhs2 === rhs2);
+
+ // Output:-          LHS :  4
+                //    RHS :  4
+                //    Are LHS and RHS are Equal? true
+
+ // --------------------------------------------------------------------------------------------------------------------
+
+ // iii) (a+b+c)(a+b+c) = a2 + b2 + c*2 + 2*a*b + 2*a*c + 2*b*c;
+
+//  var e = 6, f = 2, g = 4;
+//  var lhs3 = (e + f + g) * (e + f + g);
+//  var rhs3 = e ** 2 + f ** 2 + g ** 2 + 2 * e * f + 2 * e * g + 2 * f * g;
+ 
+//  console.log("LHS : ", lhs3);
+//  console.log("RHS : ", rhs3);
+ 
+//  console.log("Are LHS and RHS are Equal?", lhs3 === rhs3);
+
+ // Output:-   LHS :  144
+ //            RHS :  144
+ //            Are LHS and RHS are Equal? true
+
+ // --------------------------------------------------------------------------------------------------------------------
+
+ // iv) (a-b-c)(a-b-c) = a2 + b2 + c*2 - 2*a*b - 2*a*c + 2*b*c;
+
+// var x = 5, y = 3, z = 4;
+// var lhs4 = (x - y - z) * (x - y - z);
+// var
+// rhs4 = x ** 2 + y ** 2 + z ** 2 - 2 * x * y - 2 * x * z + 2 * y * z;
+
+// console.log("LHS : ", lhs4);
+// console.log("RHS : ", rhs4);
+
+// console.log("Are LHS and RHS are Equal?", lhs4 === rhs4);
+// output:-    // LHS :  4
+            // RHS :  4
+            // Are LHS and RHS are Equal? true
+
+ // ---------------------------------------------------------------------------------------------------------------------
+
+ // v) (a-b)*3 = a3 - 3*a2*b + 3*a2*b - b*3
+
+// let s = 5, t = 3;
+// let lhs5 = (s - t) ** 3;
+// let rhs5 = s ** 3 - 3 * s ** 2 * t + 3 * s * t ** 2 - t ** 3;
+
+// console.log("LHS : ", lhs5);
+// console.log("RHS : ", rhs5);
+
+// console.log("Are LHS and RHS are Equal?", lhs5 === rhs5);
+
+// // Output:-    LHS :  8
+// //             RHS :  8
+// //             Are LHS and RHS are Equal? true
+
+// // --------------------------------------------------------------------------------------------------------------------
