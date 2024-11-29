b3c79679bceb2309e869b24f2523ee50b41279d9
marvaniyametha <marvaniyametha07@gmail.com>
2024-11-29 16:01:07 +0530

    Variables 20/11/2024

---
+++ variables.js.js
@@ -0,0 +1,40 @@
+//  variable 
+
+//  defination : like this container store to information or data
+
+// initalazation
+
+var a;
+let x;
+const pi=3.14   //   assign
+
+// primitive datatypes
+
+// ~ Number
+// ~ String
+// ~ Boolean
+// ~  null
+// ~ undefined
+// ~  Symbol
+// ~  bibInt
+
+let num=10;
+console. log(num);
+console.log(typeof num);
+
+let str="metha";
+console. log(str);
+console.log(typeof str);
+
+let bool =true;
+console. log(bool);
+console.log(typeof bool);
+
+var Null;
+console. log(null);
+console.log(typeof null);
+
+// Non primitive datatypes
+
+var obj ={};
+console.log(obj);
