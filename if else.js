597d80bbcefb680f9b4d7771181dedc882cc3a72
marvaniyametha <marvaniyametha07@gmail.com>
2024-11-29 16:02:02 +0530

    If-else 22/11/2024

---
+++ if else.js
@@ -0,0 +1,57 @@
+// conditional statement 
+
+// if else,nested,ladder,switch,ternary
+/* 
+if(condition){
+    Statement...
+}else{
+    statement...
+}
+*/
+
+// examples
+
+// let num1=10,num2=25;
+// if(num1<num2){
+//     console.log("small number is:",num1);
+// }else{
+//     console.log("smaller no is:",num2)
+// }
+
+// Ternary statement
+/*
+condition?true:false
+
+*/
+// examples
+
+// let num1=29,num2=18;
+
+// num1<num2?console.log("small number is:",num1):console.log("smaller number is:",num2);
+
+// console.log("small number is:",num1<num2?num1:num2);
+
+
+
+// swapping number
+// 1. with third variable
+// 2.without third variable
+
+// let a=20,b=30,c;
+
+//  console.log("before swapping");
+// console.log("A ------>",a);
+// console.log("B ------>",b);
+
+// // c=a;(using third variable)
+// // a=b;
+// // b=c;
+
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log("after swapping");
+// console.log("A ------>",a);
+// console.log("B ------>",b);
