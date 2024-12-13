f5bc9c0139c11039057c20d9edf17491e3fe5570
marvaniyametha <marvaniyametha07@gmail.com>
2024-12-13 23:51:00 +0530

    10/12/2024    RECURSION

---
+++ recursion.js
@@ -0,0 +1,39 @@
+// Recursion Function :- A Function Call It Self Again an Again.
+
+// function sayHello(n){
+//     console.log("Inside Function called :",n);
+//     if(n < 5){
+//         sayHello(++n);
+//     }
+//     console.log("Function Finished.",n);
+// }
+// sayHello(1);
+
+// Factorial Number with Recursion Function.
+
+// function factorial(a){
+//     if(a==1){
+//         return 1;
+//     }
+//     else{
+//         return a * factorial(a-1);
+//     }
+// }
+// console.log(factorial(5));
+
+// Febonacci Number with Recursion Function. 1 1 2 3 5 8 13...
+
+// function fibonacci(n) {
+//     if (n === 0) {
+//         return 0;
+//     } else if (n === 1) {
+//         return 1;
+//     } else {
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// }
+
+// n = 10;
+// for (let i = 0; i < n; i++) {
+//     console.log(fibonacci(i));
+// }
