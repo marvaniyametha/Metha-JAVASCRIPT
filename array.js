2023d49523ec6adc666da72d059abf88db7b3226
marvaniyametha <marvaniyametha07@gmail.com>
2024-12-13 23:38:39 +0530

    array  13/12/2024

---
+++ array.js
@@ -0,0 +1,69 @@
+//array=>store multiple value in single variavble(index start with 0)s
+//let arr=[2,3,4,5,6];
+//console.log(arr);
+
+//array access => index number data Store 
+//console.log(arr[3])
+
+//types:-object ,non-primitive datatype
+//method:-check array format ture/flase
+//console.log(array.isarray(arr))
+//property array=>
+ //console.log((Array.length))
+//total number of length return 
+
+//  function or method
+//Push,pop,shift,unshift,splice
+//1.push=always index added in last
+// let data=[11,22,33,44,55]
+// console.log(data);
+// data.push(66);
+// console.log(data);
+
+// output:-
+// [ 11, 22, 33, 44, 55 ]
+// [ 11, 22, 33, 44, 55, 66 ]
+
+// //2.pop=remove last index
+// let data=[11,22,33,44,55]
+// console.log(data);
+// data.pop();
+// console.log(data);
+
+// output:-
+// [ 11, 22, 33, 44, 55 ]
+// [ 11, 22, 33, 44 ]
+
+
+//3.shift=remove first index
+// let data=[11,22,33,44,55,66]
+// console.log(data);
+// data.shift();
+// console.log(data);
+
+// output:-
+// [11, 22, 33, 44, 55, 66 ]
+// [ 22, 33, 44, 55, 66 ]
+
+//4.unshift=add first index
+// let data=[11,22,33,44,55,66]
+// console.log(data);
+// data.unshift(4)
+// console.log(data);
+// output:-
+// [ 11, 22, 33, 44, 55, 66 ]
+// [
+//    4, 11, 22, 33,
+//   44, 55, 66
+// ]
+
+//splice=remove any position
+// let data=[11,22,33,44,55]
+// console.log(data);
+// data.splice(2,2);
+// console.log(data);
+
+// output:-
+
+// [ 11, 22, 33, 44, 55 ]
+// [ 11, 22, 55 ]
