dbd2a3d4b488a2a6a0589df2061a17c5fa1a9898
marvaniyametha <marvaniyametha07@gmail.com>
2024-12-17 15:14:32 +0530

    test-4 function 17/12/2024

---
+++ function-test4.js
@@ -0,0 +1,159 @@
+// 1.write a JAVASCRIPT  program to  print a greeting message using with 
+// agruments no return  type function when call a function without argument at that time print message GOOD MORNING.
+
+// function  greetingMess(Hello) {
+//     if(Hello){
+//         console.log(Hello);
+//     }
+//     else{
+//         console.log("Good Morning !!!");
+//     }
+// }
+// greetingMess();
+
+// output:-
+// Good Morning !!!
+
+// ====================================================================================================================================================================================
+
+// 2. write a JAVASCRIPT program to find cicle area(area =pi*r*r)using return type with argument function.
+
+// Formula for area of a circle: pi * r²
+
+// function CircleArea(r) {
+//     const pi = 3.14; 
+//     return pi * r * r; 
+// }
+
+// let radius = 7; 
+
+// let area = CircleArea(radius);
+// console.log("The area of the circle is: " + area);
+
+// output:-
+// The area of the circle is: 153.86
+
+
+// ===============================================================================================================================================================
+
+// 3. write a JAVASCRIPT  program to find Triangle (area = (l * h)/2) using return with argument function.
+// formula:- (area = (l * h)/2)
+
+// function calculateTriangleArea(l, h) {
+//     return (l * h) / 2;
+// }
+// let height = 5; 
+// let base = 10; 
+// let area = calculateTriangleArea(base, height);
+// console.log("The area of the triangle is: " + area);
+
+// output:-
+// The area of the triangle is: 25
+
+// ====================================================================================================================================================================
+
+// 4. write a JAVASCRIPT program to find Rectangle area = l*h using return type with argument function.
+
+
+// function areaRectangle(length, width, rectangle) {
+//     rectangle = length * width;
+//     return rectangle;
+// }
+
+// console.log(areaRectangle(18, 18));
+
+// output:-
+// 324
+
+// ==========================================================================================================================================================================
+
+// 5.write a JAVASCRIPT program to find ans of ((b*b)(4*a*c))/(2*a) using return type with argument function.
+
+// function findSum(a, b, c) {
+//     let result = (b * b * (4 * a * c)) / (2 * a);
+//     return result;
+// }
+
+// console.log(findSum(2, 2, 2));
+
+// output:-
+// 16
+
+// ===================================================================================================================================================================
+
+// 6. Write a JAVASCRIPT Program to find ans of (a*a) + (2*a*b)+(b*b) using return type with argument function.
+
+
+// function sum(x, y) {
+//     let result = (x * x) + (2 * x * y) + (y * y);
+//     return result;
+// }
+
+// console.log(sum(5, 5));
+
+// output:-
+// 100
+
+// =====================================================================================================================================================================
+
+
+// 7.write a JAVASCRIPT program to convert Fahrenheit to Celcius (c=(f-32)/1.8) using return type with argument function.
+
+// function Fahrenheit(c,f){
+//     return c=(f-32)/1.8;
+// }
+// console.log(Fahrenheit(2,8))
+
+// output:-
+// -13.333333333333332
+
+// =========================================================================================================================================================================
+
+// 8. write a JAVASCRIPT program to convert   Celcius to Fahrenheit(f=(c*1.8)+32) using return type with argument function.
+
+// function celsius(c,f){
+//     return f=(c*1.8)+32;
+//  }
+//  console.log(celsius(5,5))
+
+//  output:-
+//  41
+
+// ==========================================================================================================================================================
+
+// 9. write a JAVASCRIPT program to find if a given number is odd or even using an argument with no return type.
+
+// function evenOdd(n) {
+//     if (n % 2 == 1) {
+//         console.log(n,"is evenNumber.");
+//     } else {
+//         console.log(n,"is oddNumber.");
+//     }
+// }
+// evenOdd(18);
+// evenOdd(3);
+
+
+// output:-
+// 18   is oddNumber.
+// 3 is evenNumber.
+
+// ==============================================================================================================================================================
+
+// 10. write a JAVASCRIPT  program to swap a value without third variable using with argument no return type.
+
+// function swapping(m,n){
+//     console.log("Before swapping ",m,n);
+//     m=m+n;
+//     n=m-n;
+//     m=m-n;
+//     console.log("After swapping",m,n);
+
+// }
+// swapping(18,19);
+
+// OUTPUT:-
+// Before swapping  18 19
+// After swapping 19 18
+
+// ============================================================================================
