5507d231cd33d413575b7440492383ca4e63dcd4
marvaniyametha <marvaniyametha07@gmail.com>
2024-12-23 16:20:16 +0530

    string 20/12/2024

---
+++ string.js
@@ -0,0 +1,83 @@
+// string:- collection of character ," " , ''.
+// let a="hello"
+// let a='how are you'
+// console.log(a)
+
+
+//string litreal (template itlreal) : "" or '' join in one sentence
+
+// let a= "helloo'i mmethaa"
+// console.log(a);
+
+// slice
+// let data ="javascript is a best language"
+// let a=data.slice(0,10);
+// console.log(a);
+
+// substring
+
+// let data ="javascript is a best language"
+// let a=data.substring(0,5);
+// console.log(a);
+
+// substr
+// let data ="javascript is a best language"
+// let a=data.substr(0,5);
+// console.log(a);
+
+
+// split(seprators)
+
+// let data ="javascript is a best language"
+// let a= data.split("is") //space count
+// console.log(a);
+
+
+// to upper case
+
+// let data ="javascript is a best language"
+// let a=data.toUpperCase()
+// console.log(a);
+
+//lower case
+
+// let data ="javascript is a best language"
+// let a= data.toLowerCase()
+// console.log(a);
+
+//Trim:- remove extra space
+//  let data ="javascript is a best language"
+// let a=data.trim()
+// console.log(a);
+
+
+// math method
+
+//math (return small value)
+
+// let a=Math.floor(17.9)
+// console.log(a);
+
+//ceil (return big value)
+
+// let a=Math.ceil(13.4)
+// console.log(a);
+
+//round  (return round value)
+
+// // let a=Math.round(5.8)
+// let h=Math.round(5.2)
+// console.log(a);
+
+//random     only o to 10 value countable
+
+// let a=Math.random()
+// console.log(a);
+   
+// 0 to 100
+
+// let a=Math.random() * 100
+// console.log(a);
+
+let date =new Date()
+console.log(date);
